0cfb68c545a21fd7ab72524643f7fd51e33c5fd8
ashish-ksolves <ashish.verma@ksolves.com>
2023-04-13 19:21:20 +0530

    Checkpoint to Angular PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,54 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-pwa';
+  todos: {title: string, completed: string}[] = [];  
+  users: {username: string, email: string}[] = [];
+
+  constructor(private http: HttpClient){
+    this.getTodos();
+    this.postRequest();
+    // this.getComments();
+    this.getPosts();
+  }
+
+  getTodos () {
+    this.http.get("https://jsonplaceholder.typicode.com/todos/").subscribe((res: any)=>{
+      console.log(res);
+      this.todos =res;
+    });
+  }
+
+  postRequest() {
+    this.http.post("https://jsonplaceholder.typicode.com/users/", {}).subscribe(res=>{
+      console.log(res);
+      // alert("POST SUCCEEDED!!!")
+    }, err=>{
+      console.log(err)
+      // alert(err.message)
+
+    });
+  }
+
+  getPosts() {
+    this.http.get("https://jsonplaceholder.typicode.com/users/").subscribe((res: any)=>{
+      console.log(res);
+      // alert("POST IS HERE")
+      this.users = res;
+    }, err=>{
+      console.log(err)
+      // alert(err.message)
+
+    });
+  }  
+  
+  // getComments() {
+  //   this.http.post("https://jsonplaceholder.typicode.com/posts/1/comments", {}).subscribe(res=>{
+  //     console.log(res);
+  //     // alert("POST COMMENTS IS HERE")
+  //   }, err=>{
+  //     console.log(err)
+  //     // alert(err.message)
+
+  //   });
+  // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
